a5aaecbfb2a24225c54c2427cde0235f4146756f
月岡祐太 <yuta_tsukioka@tsukiokayuutanoMacBook-Pro.local>
2017-05-23 14:13:41 +0900

    空白の使い方の統一、各種Writerの場所移動

--- src/main/java/com/kintai/kintaiMain.java
+++ src/main/java/com/kintai/kintaiMain.java
@@ -7,10 +7,9 @@ import java.util.HashMap;
 public class kintaiMain {
     public static void main(String[] args){
         try {
-            FileWriter fw = new FileWriter("../../../csv/kintai.csv", true);
-            PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
-
             if (args[0].equals("input")) {
+                FileWriter fw = new FileWriter("../../../csv/kintai.csv", true);
+                PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
                 pw.print(args[1]);
                 pw.print(",");
                 pw.print(args[2]);
@@ -24,7 +23,7 @@ public class kintaiMain {
                 HashMap<Integer,Integer[]> hash = new HashMap<>();
                 BufferedReader br = new BufferedReader(new FileReader("../../../csv/kintai.csv"));
                 String tmp;
-                while ( ( tmp = br.readLine()) != null){
+                while ((tmp = br.readLine()) != null){
                     String array[] = tmp.split(",");
                     Integer time[] = {Integer.parseInt(array[1]),Integer.parseInt(array[2])};
                     hash.put(Integer.parseInt(array[0]),time);
@@ -33,30 +32,30 @@ public class kintaiMain {
                 ArrayList<Integer> kinmu = new ArrayList<>();
                 ArrayList<Integer> zangyo = new ArrayList<>();
 
-                hash.forEach(( key, value) -> {
+                hash.forEach((key,value) -> {
                     if (value[1] <= 1800) {
-                        if ( value[1] <= 1200){
-                            kinmu.add(value[1]-900);
+                        if (value[1] <= 1200){
+                            kinmu.add(value[1] - 900);
                         }
                         else if (value[1] <= 1300){
                             kinmu.add(300);
-                        } else{
-                            kinmu.add(value[1]-1000);
+                        } else {
+                            kinmu.add(value[1] - 1000);
                         }
                     }
                     else{
                         kinmu.add(800);
-                        if ( value[1] <= 1900){
+                        if (value[1] <= 1900){
 
                         }
-                        else if ( value[1] <= 2100 ){
-                            zangyo.add(value[1]-1900);
+                        else if (value[1] <= 2100){
+                            zangyo.add(value[1] - 1900);
                         }
-                        else if ( value[1] <= 2200){
+                        else if (value[1] <= 2200){
                             zangyo.add(200);
                         }
-                        else if ( value[1] <= 2400){
-                            zangyo.add(value[1]-2000);
+                        else if (value[1] <= 2400){
+                            zangyo.add(value[1] - 2000);
                         }
                         else{
                             zangyo.add(400);
@@ -65,18 +64,17 @@ public class kintaiMain {
                 });
                 System.out.println("勤務時間");
                 int kinmu_total = 0;
-                for( int i = 0 ; i < kinmu.size(); i++ ){
+                for(int i = 0; i < kinmu.size(); i++){
                     kinmu_total += kinmu.get(i);
                 }
                 System.out.println(kinmu_total/100 + "時間" + kinmu_total%100 + "分");
                 System.out.println("残業時間");
                 int zangyo_total = 0;
-                for( int j = 0 ; j < zangyo.size(); j++){
+                for(int j = 0; j < zangyo.size(); j++){
                     zangyo_total += zangyo.get(j);
                 }
                 System.out.println(zangyo_total/100 + "時間" + zangyo_total%100 + "分");
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
