af81bc405f7f56856af6757a5eaa00dc8c897bed
月岡祐太 <yuta_tsukioka@tsukiokayuutanoMacBook-Pro.local>
2017-05-25 15:29:11 +0900

    add testfile

---
+++ src/test/java/com/kintai/kintaiMainTest.java
@@ -0,0 +1,12 @@
+package com.kintai;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class kintaiMainTest {
+    @Test
+    public void main() throws Exception {
+    }
+
+}
